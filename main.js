a40e933e2b12ead94879a74a13e9552e1c8cfb73
Pedro Poliszuk <124004844+pedropoliszuk@users.noreply.github.com>
2024-03-05 20:58:55 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const botaoMensagem = document.getElementById('botaoMensagem');
+    botaoMensagem.addEventListener('click', function() {
+        alert('Tenha uma ótima semana :)')
+    })
+})
