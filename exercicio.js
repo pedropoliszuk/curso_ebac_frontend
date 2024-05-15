982bbff5eb3fd21a6af39b2edda0f06d8b814b15
Pedro Poliszuk <124004844+pedropoliszuk@users.noreply.github.com>
2024-05-15 20:47:48 -0300

    Add files via upload

---
+++ exercicio.js
@@ -0,0 +1,10 @@
+"use strict";
+function multiplicacao(num1, num2) {
+    return num1 * num2;
+}
+multiplicacao(5, 3);
+function saudacao(nome) {
+    return "Olá" + nome;
+}
+;
+saudacao('Pedro');
---
+++ exercicio.ts
@@ -0,0 +1,11 @@
+function multiplicacao(num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+multiplicacao(5, 3);
+
+function saudacao(nome: string): string {
+    return "Olá" + nome;
+};
+
+saudacao('Pedro');
